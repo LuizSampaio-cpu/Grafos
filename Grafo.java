92f499c7b3fa116b4aae850112d673080368a0cd
LuizSampaio-cpu <luizhorta2910@gmail.com>
2023-05-05 09:05:51 -0300

    check

---
+++ Grafo.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+
+public class Grafo<T> {
+    private ArrayList<Aresta<T>> arestas;
+    private ArrayList<Vertice<T>> vertices;
+
+
+    public ArrayList<Aresta<T>> getArestas() {
+        return arestas;
+    }
+    public void setArestas(ArrayList<Aresta<T>> arestas) {
+        this.arestas = arestas;
+    }
+    public ArrayList<Vertice<T>> getVertices() {
+        return vertices;
+    }
+    public void setVertices(ArrayList<Vertice<T>> vertices) {
+        this.vertices = vertices;
+    }
+    
+
+    public Vertice<T> adicionarVertice(T valor){
+        Vertice<T> novo = new Vertice<T>(valor);
+        this.vertices.add(novo);
+        return novo;
+    }
+
+
+    private Vertice<T> obterVertice(T valor){
+        Vertice<T> v;
+        for(int i = 0; i < this.vertices.size(); i++){
+            v = this.vertices.get(i);
+            if(v.getValor().equals(valor)){
+                return v;
+            }
+        }
+        // chegou até aqui, significa que não achou o vértice
+        return null;
+    }
+
+    public void AddAresta(T origem, T destino, float peso){
+        adicionaAresta(origem, destino, peso);
+    }
+
+    private void adicionaAresta(T origem, T destino, float peso){
+        Vertice<T> verticeOrigem, verticeDestino;
+        Aresta<T> novaAresta;
+
+        //busca o vértice com o valor de origem
+        verticeOrigem = obterVertice(origem);
+
+        //caso não exista, cria um vértice com esse valor
+        if (verticeOrigem == null){
+           verticeOrigem = adicionarVertice(origem);
+        }
+
+        //repete o procedimento com o vértice de destino
+        verticeDestino = obterVertice(destino);
+
+        if (verticeDestino == null){
+           verticeDestino = adicionarVertice(destino);
+        }
+        
+        //cria a aresta que vai conectar os dois
+        novaAresta = new Aresta<T>(verticeOrigem, verticeDestino, peso);
+
+        // adiciona a aresta à lista de grafos
+        this.arestas.add(novaAresta);
+    }
+  
+
+    public Vertice<T> BuscaEmLargura(){
+        ArrayList<Vertice> marcados = new ArrayList<Vertice>();
+        ArrayList<Vertice> fila = new ArrayList<Vertice>();
+
+        Vertice<T> atual = this.vertices.get(0);
+        fila.add(atual);
+        
+        while(fila.size() > 0){
+            atual = fila.get(0);
+            fila.remove(0);
+            marcados.add(atual);
+            System.out.println(atual.getValor());
+            ArrayList<Aresta> destinos = this.obterDestinos(atual);
+            Vertice<T> proximo;
+
+            for(int i = 0; i < destinos.size(); i++){
+                proximo = destinos.get(i).getDestino();
+                if(!marcados.contains(proximo) && !fila.contains(proximo)){
+                    fila.add(proximo);
+                }
+            }
+        }
+    }
+
+
+    private ArrayList<Aresta> obterDestinos(Vertice<T> vertice){
+        
+    }
+}
